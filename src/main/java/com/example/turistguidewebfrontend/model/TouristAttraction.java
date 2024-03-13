fcaa3568e6ab62e7959e390daf8dfd6d84c5b730
chye0001 <chye0001@stud.kea.dk>
2024-03-13 18:52:37 +0100

    changed return type from double to int for getPrice()

--- src/main/java/com/example/turistguidewebfrontend/model/TouristAttraction.java
+++ src/main/java/com/example/turistguidewebfrontend/model/TouristAttraction.java
@@ -57,7 +57,7 @@ public class TouristAttraction {
     public List<String> getTags() {
         return tags;
     }
-    public double getPrice(){
+    public int getPrice(){
         return price;
     }
     public String getCurrencyCode() {
